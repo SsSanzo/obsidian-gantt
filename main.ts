5d770ed9f9e320f893260909e750b00474082174
SsSanzo <clem639@hotmail.com>
2021-12-26 17:25:55 -0800

    v9

--- main.ts
+++ main.ts
@@ -1,27 +1,14 @@
-import { App, MarkdownPostProcessor, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {MarkdownPostProcessor, Plugin} from 'obsidian';
 
 import {Parser} from 'src/parser';
 import {Renderer} from 'src/render';
 
 // Remember to rename these classes and interfaces!
 
-interface ISettingsData {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: ISettingsData = {
-	mySetting: 'default'
-}
-
 export default class MyPlugin extends Plugin {
-	settings: ISettingsData;
     postprocessors: Map<string, MarkdownPostProcessor> = new Map();
 
 	async onload() {
-		await this.loadSettings();
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		this.registerMarkdownCodeBlockProcessor("gantt", (source, element, context) => {
 			const parser = new Parser();
@@ -48,44 +35,5 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
